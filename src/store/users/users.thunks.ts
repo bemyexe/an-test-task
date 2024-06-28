d21e0252e02f360d35dc0e75ec710cdcdb642403
maxim <trifonov.max@mail.ru>
2024-06-28 09:00:27 +0300

    refactor(users-thunks): delete default parametr

--- src/store/users/users.thunks.ts
+++ src/store/users/users.thunks.ts
@@ -15,7 +15,7 @@ export const getUser = createAppAsyncThunk(
 
 export const getUsers = createAppAsyncThunk(
   'users/getUsers',
-  async (page: number = 0, { rejectWithValue }) => {
+  async (page: number, { rejectWithValue }) => {
     try {
       const response = await UsersService.getUsers(page);
       return response;

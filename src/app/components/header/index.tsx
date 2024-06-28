f10ade2746cbf28924b18dada45828abc72a8547
maxim <trifonov.max@mail.ru>
2024-06-28 17:33:15 +0300

    feature(header): added  Responsive Layout

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -3,7 +3,10 @@ import { useLocation } from 'react-router-dom';
 import { STRINGS } from '../../../constants/strings';
 import { useAppDispatch } from '../../../store';
 import { logout } from '../../../store/auth/auth.thunks';
+import useWindowSize from '../../hooks/use-window-size';
 import { Button } from '../shared/button';
+import { Icon } from '../shared/icon';
+import { Icons } from '../shared/icon/enum/icon-enum';
 
 import './style.scss';
 
@@ -11,20 +14,37 @@ export const Header = () => {
   const dispatch = useAppDispatch();
   const location = useLocation();
   const isMainPage = location.pathname === '/main';
+
+  const { isMobile } = useWindowSize();
+
+  const handleBackButtonClick = () => {};
+
+  const handleLogoutButtonClick = () => {
+    dispatch(logout());
+  };
+
+  const leftContent = !isMobile ? (
+    !isMainPage && (
+      <Button title={STRINGS.back} onClick={handleBackButtonClick}>
+        {STRINGS.back}
+      </Button>
+    )
+  ) : (
+    <Icon onClick={handleBackButtonClick} name={Icons.back} className="back" />
+  );
+
+  const rightContent = !isMobile ? (
+    <Button title={STRINGS.exit} onClick={handleLogoutButtonClick}>
+      {STRINGS.exit}
+    </Button>
+  ) : (
+    <Icon name={Icons.logOut} onClick={handleLogoutButtonClick} />
+  );
+
   return (
     <header className="header">
-      <div className="header-left">
-        {!isMainPage && (
-          <Button title={STRINGS.back} onClick={() => {}}>
-            {STRINGS.back}
-          </Button>
-        )}
-      </div>
-      <div className="header-right">
-        <Button title={STRINGS.exit} onClick={() => dispatch(logout())}>
-          {STRINGS.exit}
-        </Button>
-      </div>
+      <div className="header-left">{leftContent}</div>
+      <div className="header-right">{rightContent}</div>
     </header>
   );
 };

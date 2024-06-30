5ae4a999ac292f9c63d0802dad2f1a86275dfe8d
maxim <trifonov.max@mail.ru>
2024-06-30 13:50:33 +0300

    feature(users-grid): added skeleton logic for data loading

--- src/app/components/users-grid/index.tsx
+++ src/app/components/users-grid/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
 import { useAppDispatch } from '../../../store';
@@ -6,21 +6,34 @@ import { usersSelectors } from '../../../store/users/users.selectors';
 import { getUsers } from '../../../store/users/users.thunks';
 
 import { UserCard } from './user-card';
+import { UserCardSkeleton } from './user-card-skeleton';
 
 import './style.scss';
 
 export const UserGrid = () => {
   const dispatch = useAppDispatch();
   const users = useSelector(usersSelectors.selectUsersList);
+  const isUserLoading = useSelector(usersSelectors.selectUsersLoading);
+
   useEffect(() => {
-    dispatch(getUsers(1));
-  }, [dispatch]);
+    if (!users || users.length === 0) {
+      dispatch(getUsers(1));
+    }
+  }, [dispatch, users]);
+
+  const usersSkeleton = useMemo(
+    () => [...new Array(6)].map((_, i) => <UserCardSkeleton key={i} />),
+    []
+  );
+
+  const usersGrid = useMemo(
+    () => users?.map((user) => <UserCard key={user.id} {...user} />),
+    [users]
+  );
 
   return (
     <div className="users-grid">
-      {users?.map((user) => (
-        <UserCard key={user.id} {...user} />
-      ))}
+      {isUserLoading ? usersSkeleton : usersGrid}
     </div>
   );
 };

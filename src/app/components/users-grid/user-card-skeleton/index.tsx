a78b3cde1205b615a525a502931210e448c7a26e
maxim <trifonov.max@mail.ru>
2024-06-30 13:47:02 +0300

    feature(users-grid): added skeleton for users-grid

---
+++ src/app/components/users-grid/user-card-skeleton/index.tsx
@@ -0,0 +1,22 @@
+import ContentLoader from 'react-content-loader';
+
+interface SkeletonProps {
+  className?: string;
+}
+
+export const UserCardSkeleton = (props: SkeletonProps) => (
+  <ContentLoader
+    speed={2}
+    width={329}
+    height={355}
+    viewBox="0 0 329 355"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+    {...props}
+  >
+    <rect x="72" y="140" rx="0" ry="0" width="125" height="24" />
+    <rect x="195" y="206" rx="0" ry="0" width="34" height="31" />
+    <circle cx="131" cy="65" r="62" />
+    <rect x="41" y="206" rx="0" ry="0" width="102" height="33" />
+  </ContentLoader>
+);

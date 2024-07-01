e8ba8c4c145db51534bd6d38b83002254ff92620
maxim <trifonov.max@mail.ru>
2024-07-01 16:34:44 +0300

    refactor(secured-routes): added Outlet for children elements

--- src/app/routes/secured-routes/index.tsx
+++ src/app/routes/secured-routes/index.tsx
@@ -1,13 +1,12 @@
 import { useEffect } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 import StorageService from '../../../storage-services/auth-storage.service/auth-storage.service';
 import { Header } from '../../components/header';
-import { MainPage } from '../../pages/main-page';
-import { UserPage } from '../../pages/user-page';
 
 export const SecuredRoutes = () => {
   const navigate = useNavigate();
+
   useEffect(() => {
     const token = StorageService.getToken();
     if (!token) {
@@ -18,10 +17,7 @@ export const SecuredRoutes = () => {
   return (
     <div className={'secured-routes'}>
       <Header />
-      <Routes>
-        <Route path={'/main'} element={<MainPage />} />
-        <Route path={'/users/:userId'} element={<UserPage />} />
-      </Routes>
+      <Outlet />
     </div>
   );
 };

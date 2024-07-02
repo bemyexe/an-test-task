a5a1c40aee89af00c7df0e61a0a7b2b09895c490
maxim <trifonov.max@mail.ru>
2024-07-02 23:10:01 +0300

    refactor(header): Refactor header component for responsive layout and separation of concerns

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -3,7 +3,6 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { STRINGS } from '../../../constants/strings';
 import { useAppDispatch } from '../../../store';
 import { logout } from '../../../store/auth/auth.thunks';
-import useWindowSize from '../../hooks/use-window-size';
 import { Button } from '../shared/button';
 import { Icon } from '../shared/icon';
 import { Icons } from '../shared/icon/enum/icon-enum';
@@ -15,7 +14,6 @@ export const Header = () => {
   const location = useLocation();
   const isMainPage = location.pathname === '/main';
   const navigate = useNavigate();
-  const { isMobile } = useWindowSize();
 
   const handleGoBack = () => {
     navigate(-1);
@@ -26,22 +24,38 @@ export const Header = () => {
     navigate('/auth');
   };
 
-  const leftContent = isMobile
-    ? !isMainPage && (
-        <Icon onClick={handleGoBack} name={Icons.back} className="back" />
-      )
-    : !isMainPage && (
-        <Button title={STRINGS.back} onClick={handleGoBack}>
-          {STRINGS.back}
-        </Button>
-      );
+  const leftContent = !isMainPage && (
+    <>
+      <Button
+        className="header-left-back-button"
+        title={STRINGS.back}
+        onClick={handleGoBack}
+      >
+        {STRINGS.back}
+      </Button>
+      <Icon
+        onClick={handleGoBack}
+        name={Icons.back}
+        className="header-left-back-icon"
+      />
+    </>
+  );
 
-  const rightContent = isMobile ? (
-    <Icon name={Icons.logOut} onClick={handleLogout} />
-  ) : (
-    <Button title={STRINGS.exit} onClick={handleLogout}>
-      {STRINGS.exit}
-    </Button>
+  const rightContent = (
+    <>
+      <Button
+        className="header-right-logout-button"
+        title={STRINGS.exit}
+        onClick={handleLogout}
+      >
+        {STRINGS.exit}
+      </Button>
+      <Icon
+        className="header-right-logout-icon"
+        name={Icons.logOut}
+        onClick={handleLogout}
+      />
+    </>
   );
 
   return (

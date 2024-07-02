c214bb2a3bc812faa410beb425d9ee769bafc4ef
maxim <trifonov.max@mail.ru>
2024-07-02 16:33:23 +0300

    fix(secured-routes): Added loading state

--- src/app/routes/secured-routes/index.tsx
+++ src/app/routes/secured-routes/index.tsx
@@ -1,18 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 
 import StorageService from '../../../storage-services/auth-storage.service/auth-storage.service';
 import { Header } from '../../components/header';
+import { Loader } from '../../components/shared/loader';
 
 export const SecuredRoutes = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
     const token = StorageService.getToken();
     if (!token) {
       navigate('/auth');
+    } else {
+      setIsLoading(false);
     }
   }, [navigate]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
 
   return (
     <div className={'secured-routes'}>
